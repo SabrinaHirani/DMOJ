c216ca6095d4b32d84cffa081cadcfd775e382ae
SabrinaHirani <sabrinahirani@gmail.com>
2023-12-23 01:08:07 -0500

    Update

---
+++ ccc22s3.cpp
@@ -0,0 +1,48 @@
+// TODO 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    K = K-N;
+
+    int size = 0;
+
+    string result;
+
+    while (size < N) {
+
+        int x = 1;
+        int z = -1;
+        while (true) {
+            result += to_string(x)+' ';
+            size++;
+            x++;
+            z += x;
+
+            if (x > M || z >= K || size >= N) {
+                cout << (x > M) << endl;
+                cout << (z >= K) << endl;
+                cout << (size >= N) << endl;
+                break;
+            }
+
+        }
+
+        K = max(0, K-x-1);
+
+    }
+
+    if (K != 0 || size != N) {
+        cout << -1 << endl;
+        cout << result << endl;
+    } else {
+        cout << result << endl;
+    }
+
+}
---
+++ dpc.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<vector<int>> vacation(N, vector<int>(3));
+    for (int i = 0; i < N; i++) {
+        cin >> vacation[i][0] >> vacation[i][1] >> vacation[i][2];
+    }
+
+    vector<vector<int>> happiness(N, vector<int>(3, 0));
+    happiness[0][0] = vacation[0][0];
+    happiness[0][1] = vacation[0][1];
+    happiness[0][2] = vacation[0][2];
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                if (j != k) {
+                    happiness[i][j] = max(happiness[i][j], vacation[i][j]+happiness[i-1][k]);
+                }
+            }
+        }
+    }
+
+    int maximum = 0;
+    for (int i = 0; i < 3; i++) {
+        if (maximum < happiness[N-1][i]) {
+            maximum = happiness[N-1][i];
+        }
+    }
+
+    cout << maximum << endl;
+
+}
