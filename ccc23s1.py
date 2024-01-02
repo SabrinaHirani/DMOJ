d094addef546ceb81324cd98544bfeed1124734e
SabrinaHirani <sabrinahirani@gmail.com>
2024-01-02 14:31:01 -0500

    Update

--- ccc23s1.py
+++ ccc23s1.py
@@ -1,11 +1,30 @@
-# TODO complete
-
 count = 0
-hcount = 0
-vcount = 0
 
 C = int(input())
 row1 = input().split()
 row2 = input().split()
 
+prev = False
+for i in range(C):
+    if not prev and row1[i] == '1':
+        count += 3
+        prev = True
+    elif row1[i] == '1':
+        count += 1
+    else:
+        prev = False
 
+prev = False
+for i in range(C):
+    if not prev and row2[i] == '1':
+        count += 3
+        prev = True
+    elif row2[i] == '1':
+        count += 1
+    else:
+        prev = False
+
+    if i%2 == 0 and row2[i] == '1' and row1[i] == '1':
+        count -= 2
+
+print(count)
