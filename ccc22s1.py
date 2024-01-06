dc277de398f632e6f6b19ddae4dbfd9c15fdef94
SabrinaHirani <sabrinahirani@gmail.com>
2024-01-06 17:27:35 -0500

    Update

--- ccc22s1.py
+++ ccc22s1.py
@@ -1,3 +1,25 @@
+'''
+inspiration:
+
+def T(n):
+    if n < 4: 
+        return 0
+    elif n == 4:
+        return 1
+    elif n == 5:
+        return 1
+    elif n == 9:
+        return 1
+    elif n > 9:
+        return T(n-4) + T(n-5) - T(n-9)
+    else:
+        return T(n-4) + T(n-5)
+    
+n = int(input())
+print(T(n))
+
+'''
+
 n = int(input())
 
 dp = [0]*(max(5+1, n+1))
